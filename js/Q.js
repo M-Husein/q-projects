99c38b3d243c2d9432032a3a0bd2e13a9e391867
Muhamad Husein <m.husein27@gmail.com>
2018-01-12 09:42:52 +0700

    Update Q.js

--- js/Q.js
+++ js/Q.js
@@ -3,15 +3,15 @@ $(function(){
 		e.preventDefault();
 	});
 	
-	$('#navTop').on('shown.bs.collapse',function(){
+	$('#navTop,#nav2').on('shown.bs.collapse',function(){
 	 	$('<div class="modal-backdrop fade show" id="dropSideNav"></div>').appendTo('body');
 	});
-	$('#navTop').on('hidden.bs.collapse',function(){
+	$('#navTop,#nav2').on('hidden.bs.collapse',function(){
 	 	$('#dropSideNav').remove();
 	});
 
-	$(document).on('click','#dropSideNav,#navMain',function(){
-		$('#navTop').collapse('hide');
+	$(document).on('click','#dropSideNav,#navMain,#navMore',function(){
+		$('#navTop,#nav2').collapse('hide');
 	});
 	
 	
